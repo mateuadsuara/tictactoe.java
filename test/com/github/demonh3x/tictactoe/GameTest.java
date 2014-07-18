851bac5487631722b6f0c43600d4791131bafadd
mateu <mateu@touchiteasy.com>
2014-07-18 18:02:21 +0200

    Refactored game initialization

--- test/com/github/demonh3x/tictactoe/GameTest.java
+++ test/com/github/demonh3x/tictactoe/GameTest.java
@@ -1,20 +1,26 @@
 package com.github.demonh3x.tictactoe;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
 public class GameTest {
+    private Game g;
+
+    @Before
+    public void GivenANewGame() {
+        g = new Game();
+    }
+
     @Test
     public void ThereIsNoWinner() {
-        Game g = new Game();
         assertThat(g.hasAWinner(), is(false));
     }
 
     @Test
     public void IsNotADraw() {
-        Game g = new Game();
         assertThat(g.isADraw(), is(false));
     }
 }
