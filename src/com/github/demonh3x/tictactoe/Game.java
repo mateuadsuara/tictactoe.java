5cad320e0a5e7278badbacc39b3d2fc041e02767
mateu <mateu@touchiteasy.com>
2014-07-18 17:59:05 +0200

    Added initial Game.hasAWinner() behaviour

--- src/com/github/demonh3x/tictactoe/Game.java
+++ src/com/github/demonh3x/tictactoe/Game.java
@@ -1,4 +1,7 @@
 package com.github.demonh3x.tictactoe;
 
 public class Game {
+    public Boolean hasAWinner() {
+        return false;
+    }
 }
--- test/com/github/demonh3x/tictactoe/GameTest.java
+++ test/com/github/demonh3x/tictactoe/GameTest.java
@@ -2,9 +2,13 @@ package com.github.demonh3x.tictactoe;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 public class GameTest {
     @Test
-    public void createInstance(){
+    public void ThereIsNoWinner() {
         Game g = new Game();
+        assertThat(g.hasAWinner(), is(false));
     }
 }
