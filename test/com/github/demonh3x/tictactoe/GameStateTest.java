15a8c5176293a40989e58fd343fe78bb3e3b5d98
mateu <mateu@touchiteasy.com>
2014-07-20 12:38:32 +0200

    Fixed YAGNI violation with different Piece instance creation in GameStateTest

--- test/com/github/demonh3x/tictactoe/GameStateTest.java
+++ test/com/github/demonh3x/tictactoe/GameStateTest.java
@@ -5,29 +5,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
 @RunWith(HierarchicalContextRunner.class)
 public class GameStateTest {
-    private static final Piece xPieceInstance = new Piece();
-    private static final Piece oPieceInstance = new Piece();
+    private static final Piece _ = null;
+    private static final Piece X = new Piece();
+    private static final Piece O = new Piece();
 
-    private static final Supplier<Piece> _ = () -> null;
-    private static final Supplier<Piece> X = () -> xPieceInstance;
-    private static final Supplier<Piece> O = () -> oPieceInstance;
-
-    @SafeVarargs
-    private final GameState createGameState(Supplier<Piece>... suppliers) {
-        List<Piece> pieces = Arrays.asList(suppliers).stream()
-                .map((pieceSupplier) -> pieceSupplier.get())
-                .collect(Collectors.toList());
-
-        return new GameState(pieces);
+    private GameState createGameState(Piece... pieces) {
+        return new GameState(Arrays.asList(pieces));
     }
 
     public class IsNotFinished {
