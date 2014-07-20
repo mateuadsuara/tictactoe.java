56a76824f96fc020a6e21b532e38d05bd64fbffe
mateu <mateu@touchiteasy.com>
2014-07-20 16:14:48 +0200

    Generalized GameState.hasALine() code to check more than one line.

--- src/com/github/demonh3x/tictactoe/GameState.java
+++ src/com/github/demonh3x/tictactoe/GameState.java
@@ -1,5 +1,7 @@
 package com.github.demonh3x.tictactoe;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GameState {
@@ -22,8 +24,38 @@ public class GameState {
     }
 
     private boolean hasALine() {
-        return pieces.get(6) == pieces.get(7) &&
-                pieces.get(7) == pieces.get(8) &&
-                pieces.get(8) != null;
+        List<List<Integer>> possibleLines = Arrays.asList(
+                Arrays.asList(6, 7, 8),
+                Arrays.asList(2, 4, 6)
+        );
+
+        for (List<Integer> line : possibleLines)
+            if (isAWinningLine(line))
+                return true;
+
+        return false;
+    }
+
+    private boolean isAWinningLine(List<Integer> line) {
+        List<Piece> piecesInTheLine = getPiecesInTheLine(line);
+        Piece firstPiece = piecesInTheLine.get(0);
+
+        if (firstPiece == null)
+            return false;
+
+        for (Piece p : piecesInTheLine)
+            if (p != firstPiece)
+                return false;
+
+        return true;
+    }
+
+    private List<Piece> getPiecesInTheLine(List<Integer> line) {
+        ArrayList<Piece> linePieces = new ArrayList<>(line.size());
+
+        for (Integer index : line)
+            linePieces.add(pieces.get(index));
+
+        return linePieces;
     }
 }
