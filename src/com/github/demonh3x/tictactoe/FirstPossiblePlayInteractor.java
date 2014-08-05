4524ea7bf89a3d941e8684e0e4949a891d0110ab
mateu <mateu@touchiteasy.com>
2014-08-05 18:05:45 +0200

    Refactored to allow new requirements

--- src/com/github/demonh3x/tictactoe/FirstPossiblePlayInteractor.java
+++ src/com/github/demonh3x/tictactoe/FirstPossiblePlayInteractor.java
@@ -3,10 +3,13 @@ package com.github.demonh3x.tictactoe;
 public class FirstPossiblePlayInteractor implements GameInteractor {
     @Override
     public Location play(GameState state) {
-        int x = 0;
+        int index = 0;
+        int x;
 
-        while (!state.isEmptyAt(new Location(x, 0)))
-            x++;
+        do {
+            x = index;
+            index++;
+        } while (!state.isEmptyAt(new Location(x, 0)));
 
         return new Location(x, 0);
     }
