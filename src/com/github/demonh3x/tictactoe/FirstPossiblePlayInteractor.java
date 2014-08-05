2e0e2d43bbf289459d6084ff77c4c1cc925b15a9
mateu <mateu@touchiteasy.com>
2014-08-05 17:40:20 +0200

    Extracted method in FirstPossiblePlayInteractor to enhance meaning of if statement

--- src/com/github/demonh3x/tictactoe/FirstPossiblePlayInteractor.java
+++ src/com/github/demonh3x/tictactoe/FirstPossiblePlayInteractor.java
@@ -5,9 +5,13 @@ public class FirstPossiblePlayInteractor implements GameInteractor {
     public Location play(GameState state) {
         final Location firstLocation = new Location(0, 0);
 
-        if (state.lookAt(firstLocation) == null)
+        if (isEmpty(state, firstLocation))
             return firstLocation;
 
         return new Location(1, 0);
+    }
+
+    private boolean isEmpty(GameState state, Location firstLocation) {
+        return state.lookAt(firstLocation) == null;
     }
 }
