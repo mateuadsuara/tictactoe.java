a9c7027c6b2d4ea0595b7b5469feb267f0400620
mateu <mateu@touchiteasy.com>
2014-08-05 18:29:51 +0200

    Use all the Locations defined in Location.getAll() instead of generating them

--- src/com/github/demonh3x/tictactoe/FirstPossiblePlayInteractor.java
+++ src/com/github/demonh3x/tictactoe/FirstPossiblePlayInteractor.java
@@ -3,21 +3,10 @@ package com.github.demonh3x.tictactoe;
 public class FirstPossiblePlayInteractor implements GameInteractor {
     @Override
     public Location play(GameState state) {
-        int index = 0;
-        Location l;
+        for (Location l : Location.getAll())
+            if (state.isEmptyAt(l))
+                return l;
 
-        do {
-            l = createLocation(index);
-            index++;
-        } while (!state.isEmptyAt(l));
-
-        return l;
-    }
-
-    private Location createLocation(int index) {
-        int x, y;
-        x = index % Location.COLUMNS;
-        y = index / Location.COLUMNS;
-        return new Location(x, y);
+        throw new IllegalArgumentException();
     }
 }
