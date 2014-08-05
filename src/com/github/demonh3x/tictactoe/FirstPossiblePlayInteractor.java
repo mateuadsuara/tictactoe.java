a88e91779d086abfee9ef9b97b295dc1c41ff0f3
mateu <mateu@touchiteasy.com>
2014-08-05 18:09:07 +0200

    Extracted Location instantiation in FirstPossiblePlayInteractor

--- src/com/github/demonh3x/tictactoe/FirstPossiblePlayInteractor.java
+++ src/com/github/demonh3x/tictactoe/FirstPossiblePlayInteractor.java
@@ -5,14 +5,16 @@ public class FirstPossiblePlayInteractor implements GameInteractor {
     public Location play(GameState state) {
         int index = 0;
         int x, y;
+        Location l;
 
         do {
             x = index % Location.COLUMNS;
             y = index / Location.COLUMNS;
+            l = new Location(x, y);
 
             index++;
-        } while (!state.isEmptyAt(new Location(x, y)));
+        } while (!state.isEmptyAt(l));
 
-        return new Location(x, y);
+        return l;
     }
 }
