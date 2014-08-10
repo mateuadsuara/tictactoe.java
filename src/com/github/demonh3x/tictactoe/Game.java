2c796994f40f96494694b65c3295bcbd1ed66aba
mateu <mateu@touchiteasy.com>
2014-08-10 19:47:07 +0200

    Pass the first test of the usage of GameInteractors in Game

--- src/com/github/demonh3x/tictactoe/Game.java
+++ src/com/github/demonh3x/tictactoe/Game.java
@@ -4,14 +4,18 @@ import java.util.List;
 import java.util.Map;
 
 public class Game {
+    private final Map<Player, GameInteractor> interactors;
     private final List<GameObserver> observers;
 
     public Game(Map<Player, GameInteractor> interactors, List<GameObserver> observers) {
+        this.interactors = interactors;
         this.observers = observers;
     }
 
     public void run() {
         for (GameObserver observer : observers)
             observer.update(GameState.empty());
+
+        interactors.entrySet().iterator().next().getValue().play(GameState.empty());
     }
 }
