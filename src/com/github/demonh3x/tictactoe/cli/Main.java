b52b5d1aa03784fbe4db8cef8144866b90cc7cda
mateu <mateu@touchiteasy.com>
2014-08-13 20:13:55 +0200

    Made game playable in main (First make it work, then make it right...)

--- src/com/github/demonh3x/tictactoe/cli/Main.java
+++ src/com/github/demonh3x/tictactoe/cli/Main.java
@@ -1,11 +1,9 @@
 package com.github.demonh3x.tictactoe.cli;
 
-import com.github.demonh3x.tictactoe.GameState;
-import com.github.demonh3x.tictactoe.Location;
-import com.github.demonh3x.tictactoe.Player;
+import com.github.demonh3x.tictactoe.*;
+import com.github.demonh3x.tictactoe.ai.FirstPossiblePlayInteractor;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String ... args){
@@ -16,14 +14,59 @@ public class Main {
         mappings.put(xPlayer, 'X');
         mappings.put(oPlayer, 'O');
 
-        final GameState gameState = GameState.empty()
-                .put(xPlayer, new Location(0, 0))
-                .put(oPlayer, new Location(1, 0));
+        gameState = GameState.empty();
 
+        observers = new ArrayList<>();
         final CliObserver cliObserver = new CliObserver(System.out, new TextRenderer(mappings));
-        final HumanCliInteractor cliInteractor = new HumanCliInteractor(System.out, System.in);
+        observers.add(cliObserver);
 
-        cliObserver.update(gameState);
-        cliInteractor.play(gameState);
+        final GameInteractor cliInteractor = new HumanCliInteractor(System.out, System.in);
+        final GameInteractor aiInteractor = new FirstPossiblePlayInteractor();
+
+        interactors = new HashMap<>();
+        interactors.put(xPlayer, cliInteractor);
+        interactors.put(oPlayer, aiInteractor);
+
+        List<Player> playerOrder = new LinkedList<>();
+        playerOrder.add(xPlayer);
+        playerOrder.add(oPlayer);
+        players = new CyclingIterator<>(playerOrder);
+
+        run();
+    }
+
+    private static Map<Player, GameInteractor> interactors;
+    private static GameState gameState;
+    private static List<GameObserver> observers;
+    private static Iterator<Player> players;
+
+    private static void start() {
+        update();
+    }
+
+    private static void run() {
+        start();
+        while(!isFinished()){
+            tick();
+        }
+    }
+
+    private static boolean isFinished() {
+        return new GameLogic(gameState).isFinished() || !players.hasNext();
+    }
+
+    private static void tick() {
+        gameState = askToPlay(players.next());
+        update();
+    }
+
+    private static void update() {
+        for (GameObserver o : observers){
+            o.update(gameState);
+        }
+    }
+
+    private static GameState askToPlay(Player p) {
+        return gameState.put(p, interactors.get(p).play(gameState));
     }
 }
