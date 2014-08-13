d69fb70f32f7289b680fcdae65714ae81090dbae
mateu <mateu@touchiteasy.com>
2014-08-13 21:30:10 +0200

    Added initial test for GameIterator

---
+++ src/com/github/demonh3x/tictactoe/GameIterator.java
@@ -0,0 +1,23 @@
+package com.github.demonh3x.tictactoe;
+
+import java.util.Iterator;
+import java.util.Map;
+
+public class GameIterator implements Iterator<GameState> {
+    public GameIterator(GameState initial, Map<Player, GameInteractor> interactors, Iterator<Player> turns) {
+    }
+
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public GameState next() {
+        return null;
+    }
+
+    @Override
+    public void remove() {
+
+    }
+}
---
+++ test/com/github/demonh3x/tictactoe/GameIteratorTest.java
@@ -0,0 +1,54 @@
+package com.github.demonh3x.tictactoe;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.assertFalse;
+
+public class GameIteratorTest {
+    public class GameInteractorSpy implements GameInteractor {
+        public Location locationToPlay;
+        public List<GameState> receivedStatesToPlay = new ArrayList<>();
+
+        @Override
+        public Location play(GameState state) {
+            receivedStatesToPlay.add(state);
+            return locationToPlay;
+        }
+    }
+
+    private Iterator<Player> iterate(Player... players) {
+        final LinkedList<Player> list = new LinkedList<>();
+        Collections.addAll(list, players);
+        return list.iterator();
+    }
+
+    Player xPlayer = new Player();
+    Player oPlayer = new Player();
+
+    GameInteractor xInteractor;
+    GameInteractor oInteractor;
+
+    Map<Player, GameInteractor> interactors;
+
+    @Before
+    public void setUp() {
+        xPlayer = new Player();
+        oPlayer = new Player();
+
+        xInteractor = new GameInteractorSpy();
+        oInteractor = new GameInteractorSpy();
+
+        interactors = new HashMap<>();
+        interactors.put(xPlayer, xInteractor);
+        interactors.put(oPlayer, oInteractor);
+    }
+
+    @Test
+    public void GivenAnEmptyPlayerIterator_ShouldNotHaveNextState() {
+        final GameIterator game = new GameIterator(GameState.empty(), interactors, iterate());
+        assertFalse(game.hasNext());
+    }
+}
