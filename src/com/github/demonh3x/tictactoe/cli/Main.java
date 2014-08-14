7c68476489c037dd9021bf0b51cc2a3ecabf65b9
mateu <mateu@touchiteasy.com>
2014-08-14 16:40:21 +0200

    Updated Main to use the new GameIterator

--- src/com/github/demonh3x/tictactoe/cli/Main.java
+++ src/com/github/demonh3x/tictactoe/cli/Main.java
@@ -14,59 +14,35 @@ public class Main {
         mappings.put(xPlayer, 'X');
         mappings.put(oPlayer, 'O');
 
-        gameState = GameState.empty();
-
-        observers = new ArrayList<>();
+        final List<GameObserver> observers = new ArrayList<>();
         final CliObserver cliObserver = new CliObserver(System.out, new TextRenderer(mappings));
         observers.add(cliObserver);
 
         final GameInteractor cliInteractor = new HumanCliInteractor(System.out, System.in);
         final GameInteractor aiInteractor = new FirstPossiblePlayInteractor();
 
-        interactors = new HashMap<>();
+        final Map<Player, GameInteractor> interactors = new HashMap<>();
         interactors.put(xPlayer, cliInteractor);
         interactors.put(oPlayer, aiInteractor);
 
         List<Player> playerOrder = new LinkedList<>();
         playerOrder.add(xPlayer);
         playerOrder.add(oPlayer);
-        players = new CyclingIterator<>(playerOrder);
+        final Iterator<Player> players = new CyclingIterator<>(playerOrder);
 
-        run();
-    }
+        final GameState initialState = GameState.empty();
+        final GameIterator iterator = new GameIterator(initialState, interactors, players);
 
-    private static Map<Player, GameInteractor> interactors;
-    private static GameState gameState;
-    private static List<GameObserver> observers;
-    private static Iterator<Player> players;
-
-    private static void start() {
-        update();
-    }
-
-    private static void run() {
-        start();
-        while(!isFinished()){
-            tick();
+        update(observers, initialState);
+        while(iterator.hasNext()){
+            final GameState state = iterator.next();
+            update(observers, state);
         }
     }
 
-    private static boolean isFinished() {
-        return new GameLogic(gameState).isFinished() || !players.hasNext();
-    }
-
-    private static void tick() {
-        gameState = askToPlay(players.next());
-        update();
-    }
-
-    private static void update() {
+    private static void update(List<GameObserver> observers, GameState gameState) {
         for (GameObserver o : observers){
             o.update(gameState);
         }
-    }
-
-    private static GameState askToPlay(Player p) {
-        return gameState.put(p, interactors.get(p).play(gameState));
     }
 }
