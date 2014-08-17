f373dfde306eea8cfc1a199b590277664ed6a49f
mateu <mateu@touchiteasy.com>
2014-08-17 19:27:43 +0200

    Refactored virtual test in StatePossibilitiesForesighterTest as assertPossibilities method

--- test/com/github/demonh3x/tictactoe/ai/decision_making/StatePossibilitiesForesighterTest.java
+++ test/com/github/demonh3x/tictactoe/ai/decision_making/StatePossibilitiesForesighterTest.java
@@ -5,6 +5,7 @@ import com.github.demonh3x.tictactoe.game.Player;
 import com.github.demonh3x.tictactoe.game.State;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -16,60 +17,61 @@ public class StatePossibilitiesForesighterTest {
     private static final Player O = new Player();
     private static final Player _ = null;
 
+    private void assertPossibilities(Player playingNext, State initial, List<State> expectedPossibilities){
+        final StatePossibilitiesForesighter foresighter = new StatePossibilitiesForesighter(playingNext);
+        final List<State> actualPossibilities = foresighter.foresee(initial);
+
+        assertThat(actualPossibilities.size(), is(expectedPossibilities.size()));
+        for (State state : expectedPossibilities){
+            assertThat(actualPossibilities, hasItem(state));
+        }
+    }
+
     @Test
     public void GivenAFinishedState_ItHasNoPossibilities() {
-        StatePossibilitiesForesighter foresighter = new StatePossibilitiesForesighter(O);
         State finishedState = StateLiteral.create(
                 X, O, X,
                 O, X, X,
                 O, X, O
         );
-
-        final List<State> possibilities = foresighter.foresee(finishedState);
-
-        assertThat(possibilities.size(), is(0));
+        final List<State> noPossibilities = Arrays.<State>asList();
+        assertPossibilities(O, finishedState, noPossibilities);
     }
 
     @Test
     public void GivenAStateWithOnlyOnePlayRemaining_ItHasOnlyThePossibilityToFinishIt() {
-        StatePossibilitiesForesighter foresighter = new StatePossibilitiesForesighter(X);
         State onlyOnePlayRemaining = StateLiteral.create(
                 X, O, X,
                 O, X, X,
                 O, _, O
         );
-
-        final List<State> possibilities = foresighter.foresee(onlyOnePlayRemaining);
-
-        assertThat(possibilities.size(), is(1));
-        assertThat(possibilities, hasItem(StateLiteral.create(
+        final State finishedState = StateLiteral.create(
                 X, O, X,
                 O, X, X,
                 O, X, O
-        )));
+        );
+        assertPossibilities(X, onlyOnePlayRemaining, Arrays.asList(finishedState));
     }
 
     @Test
     public void GivenAStateWithTwoPlaysRemaining_ItHasTheTwoPossibleStates() {
-        StatePossibilitiesForesighter foresighter = new StatePossibilitiesForesighter(O);
-        State onlyOnePlayRemaining = StateLiteral.create(
+        final State twoPlaysRemaining = StateLiteral.create(
                 X, O, X,
                 O, X, X,
                 _, _, O
         );
-
-        final List<State> possibilities = foresighter.foresee(onlyOnePlayRemaining);
-
-        assertThat(possibilities.size(), is(2));
-        assertThat(possibilities, hasItem(StateLiteral.create(
-                X, O, X,
-                O, X, X,
-                O, _, O
-        )));
-        assertThat(possibilities, hasItem(StateLiteral.create(
-                X, O, X,
-                O, X, X,
-                _, O, O
-        )));
+        final List<State> possibleStates = Arrays.asList(
+                StateLiteral.create(
+                        X, O, X,
+                        O, X, X,
+                        O, _, O
+                ),
+                StateLiteral.create(
+                        X, O, X,
+                        O, X, X,
+                        _, O, O
+                )
+        );
+        assertPossibilities(O, twoPlaysRemaining, possibleStates);
     }
 }
