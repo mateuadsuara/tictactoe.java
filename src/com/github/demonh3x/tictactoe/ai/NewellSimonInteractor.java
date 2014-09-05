f93e167dea5eb7788d959c95a4f5629e6262caef
mateu <mateu@touchiteasy.com>
2014-09-05 21:04:35 +0200

    Extracted the BlockOption from LocationDecision

--- src/com/github/demonh3x/tictactoe/ai/NewellSimonInteractor.java
+++ src/com/github/demonh3x/tictactoe/ai/NewellSimonInteractor.java
@@ -42,6 +42,24 @@ public class NewellSimonInteractor implements Interactor {
         }
     }
 
+    private static class BlockOption implements MoveOption {
+        private final Results losingLocations;
+
+        private BlockOption(State state, Player opponent) {
+             losingLocations = new Results(state, Location.getAll()).available().winnableBy(opponent);
+        }
+
+        @Override
+        public boolean isAvailable() {
+            return losingLocations.exist();
+        }
+
+        @Override
+        public Location getLocation() {
+            return losingLocations.first();
+        }
+    }
+
     private static class LocationDecision {
         private final Player player;
         private final Player opponent;
@@ -66,9 +84,9 @@ public class NewellSimonInteractor implements Interactor {
             if (win.isAvailable())
                 return win.getLocation();
 
-            final Results losingLocations = availableLocations.winnableBy(opponent);
-            if (losingLocations.exist())
-                return losingLocations.first();
+            final MoveOption block = new BlockOption(state, opponent);
+            if (block.isAvailable())
+                return block.getLocation();
 
             final Results ownForkLocations = availableLocations.forkableBy(player);
             if (ownForkLocations.exist())
